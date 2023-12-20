d8276b7db19b4e23037185977a6168104b33057a
Aidan Ranney <aidan.s.ranney@gmail.com>
2023-12-20 15:25:17 -0800

    Track user and user node presence on homepage

--- lib/cluster_app_web/live/homepage_live.ex
+++ lib/cluster_app_web/live/homepage_live.ex
@@ -2,19 +2,43 @@ defmodule ClusterAppWeb.HomePageLive do
   use ClusterAppWeb, :live_view
 
   alias ClusterApp.AnonymousUser
+  alias ClusterAppWeb.Presence
 
 
+  @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    {:ok, assign_async(socket, :anonymous_user, fn -> {:ok, %{anonymous_user: create_anonymous_user()}} end)}
+    socket =
+      if connected?(socket) do
+        user = create_anonymous_user()
+        ClusterAppWeb.Endpoint.subscribe("lv:homepage")
+        {:ok, _} = Presence.track(self(), "lv:homepage", user.id, %{user_id: user.id, name: user.name, cursor_x: 0, cursor_y: 0, node: Node.self()})
+
+        assign(socket, :anonymous_user, user)
+      else
+        assign(socket, :anonymous_user, %AnonymousUser{})
+      end
+
+    {:ok, assign(socket, :users, [])}
   end
 
+  @impl Phoenix.LiveView
+  def handle_info(%{event: "presence_diff", payload: %{joins: joins, leaves: leaves}}, socket) do
+    users =
+      socket.assigns.users
+      |> join_users(joins)
+      |> leave_users(leaves)
+
+    socket = assign(socket, :users, users)
+
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div class="w-full">
+    <div class="w-full space-y-4">
       <div>
-        <.async_result :let={user} assign={@anonymous_user}>
-          <%= user.name %> - <%= inspect(Node.self()) %>
-        </.async_result>
+        <p>Current Node - <%= inspect(Node.self()) %></p>
       </div>
       <div>
         <p>Other nodes:</p>
@@ -24,11 +48,31 @@ defmodule ClusterAppWeb.HomePageLive do
           <% end %>
         </ul>
       </div>
+      <div>
+        <p>All Users (including current):</p>
+        <ul class="space-y-2">
+          <%= for user <- @users do %>
+            <li><%= user.name %> - <%= inspect(user.node) %></li>
+          <% end %>
+        </ul>
+      </div>
     </div>
     """
   end
 
   defp create_anonymous_user do
     struct!(AnonymousUser, %{id: Faker.UUID.v4(), name: Faker.Person.name()})
+  end
+
+  defp join_users(users, joins) do
+   joined_users = Enum.flat_map(joins, fn join ->
+    join |> elem(1) |> Map.fetch!(:metas)
+   end)
+
+   Enum.concat(joined_users, users)
+  end
+
+  defp leave_users(users, leaves) do
+    Enum.reject(users, & &1.user_id == Map.keys(leaves))
   end
 end
